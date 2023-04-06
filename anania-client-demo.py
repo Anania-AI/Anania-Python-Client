6740f56e723115a4b164316a7e9591cf17b09318
Metric <48058391+Metricam@users.noreply.github.com>
2023-04-06 10:26:05 +0400

    Updated creation flow

--- anania-client-demo.py
+++ anania-client-demo.py
@@ -3,9 +3,11 @@ from anania import Config, Create, Ask
 Config.API_KEY = "YOUR API KEY"
 
 ## Using existing project
-Config.PROJECT_KEY = "YOUR PROJECT KEY"
-Ask.ask_document("YOUR QUESTION")
+Ask.ask_document(question = "YOUR QUESTION", project_key = "YOUR PROJECT KEY")
 
 ## Creating a new project and using it
-Create.create_project()
-Ask.ask_chat("YOUR QUESTION")
+project_key_url = Create.create_project(project_name = "YOUR PROJECT NAME",
+                                    input_type=Config.TYPE_URL,
+                                    urls = ["https://metric.am/"],
+                                    crawl = False)
+Ask.ask_document("YOUR QUESTION", project_key = project_key_url)
