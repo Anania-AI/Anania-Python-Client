6e57f60f684b31e74b7e3a12f9a8ac4d5c02acf1
Metric <48058391+Metricam@users.noreply.github.com>
2023-04-06 10:26:32 +0400

    Update anania.py

--- anania.py
+++ anania.py
@@ -1,213 +1,113 @@
 import requests
-import pandas as pd
-from typing import Union
-from dataclasses import dataclass
 
-@dataclass
-class Answer:
-    
-    """
-    DataClass for easier access to Anania's answers.
-    """
-    question_id: str
-    status: str
-    sql: str
-    result: Union[str, int, pd.DataFrame]
-    chart: dict
-        
+class Config():
 
-##WARNING: none of the functions is actually checking the request status
-class Anania:
-    
-    """
-    Python Client to Anania's API. Handles Project creation and Q&A for registered users with API Key.
-    
-    Arguments:
-        API_KEY (str): API key for the user. Available on the user interface.
-        PROJECT_KEY (str): project key. will be automatically setup once create_project() is called.
-
-    """
-    
     API_KEY = None
     
-    _API_BASE = "https://anania-api.herokuapp.com/v1/"
+    ## ENDPOINTS
+    _API_BASE = "https://api.anania.ai/v1/"
     _ENDPOINT_TRAINING = f"{_API_BASE}training"
     _ENDPOINT_USER = f"{_API_BASE}user/get_information"
     _ENDPOINT_QUESTION = f"{_API_BASE}question"
+    _ENDPOINT_QUESTION_DOC = f"{_API_BASE}question/doc"
     _ENDPOINT_METADATA = f"{_API_BASE}metadata"
     _ENDPOINT_DB_CHECK = f"{_API_BASE}get_all_tables"
     _ENDPOINT_QUESTION_FLAG = f"{_API_BASE}question/flag"
     _ENDPOINT_DELETE_PROJECT = f"{_API_BASE}project"
     _ENDPOINT_UPDATE_API_KEY = f"{_API_BASE}user/new_api_key"
 
-    
-    def __init__(self, project_key=None):
-        self.question_id=None
-        self.PROJECT_KEY = project_key
-            
-    
-    ##WARNING: need to add check on wherher API_KEY is provided
-    def create_project(self,input_type: str,project_name: str, connection_string=None,table_names=None,data=None):
-        
-        """
-        Creates projects from db or via file upload.
-        In case of db connection db connection string and table_names should be provided, othervise the data in the given formats should be given as input.
-        Gets the data from the given source and Posts to Anania's server by creating a Project and returning its key.
-        
-        Args:
-            input_type (str): Which of the possible types of data input is chosen. Possible values are ['db','csv','json']
-            project_name (str): Project name 
-            connection_string (str): DB connection string
-            table_names (list): List of tables that are chosen to be included in the projects.Maximum number of tables is 3.
-            data (pd.DataFrame,dict): Dataset/table to be analyzed.Defaults to None.
-            
-        Returns:
-            PROJECT_KEY (str): The Project Key for the newly created project as an Attribute.
-        """
-        
-        headers = {"api_key":self.API_KEY} 
-        if input_type =='db':
-            body = {"project_name":project_name,"input_type":input_type,'connection_string':connection_string,'table_names':table_names}
-        else:
-            if isinstance(data,pd.DataFrame):
-                data = data.to_json(orient="records")
-            body = {"project_name":project_name,"input_type":input_type,'data':data}
-        response = requests.post(url=Anania._ENDPOINT_TRAINING,headers=headers,json=body)
-        response = response.json()
-        if response["Status"]=="Success":
-            self.PROJECT_KEY = response["Output"]
-            return self.PROJECT_KEY
-        else:
-            return response
-    
-    def get_my_project_ids(self,email: str,user_id: str):
-        
-        """
-        Uses API KEY to provide IDs of all non-demo projects created by the User.
-        Args:
-            email (str): user email address
-            user_id: user id
-        Returns:
-            PROJECT_KEY (str): The Project Key for the newly created project as an Attribute.
-        """
-        
-        headers = {"api_key":self.API_KEY}
-        body = {"mail":email,"user_id": user_id}
-        response = requests.post(url=Anania._ENDPOINT_USER,headers=headers,json=body)
-        project_keys = [i['project_key'] for i in response.json()["user_projects"]]
-        return project_keys
-    
-    ##WARNING: need to add check on wherher API_KEY and PROJECT_KEY are provided
-    def ask_question(self,question):
-        
-        """
-        Sends questions and Project Key to API and returns the answer as a response.
-        
-        Args:
-            question (str): Question in plain English that is asked to Anania.
-        
-        Returns:
-            anania.Answer: The API response including Anania's answer in "Answer" DataClass.        
-        """
-        
-        headers = {"api_key":self.API_KEY}
-        body = {"project_key":self.PROJECT_KEY,"question":question}
-        response = requests.post(url=self._ENDPOINT_QUESTION,headers=headers,json=body)
-        if response.status_code ==200:
+    ## PROJECT TYPES
+    TYPE_URL = "url_QA"
+    TYPE_CHAT = "chat_QA"
+    TYPE_PDF = "pdf_QA"
+    TYPE_CSV = "csv"
+    TYPE_DB = "db"
+
+
+class Create(Config):
+
+    def get_all_db_tables(connection_string, ssl=False):
+        headers = {"api_key": Config.API_KEY} 
+        body = {connection_string: connection_string, ssl: ssl}
+
+        response = requests.post(url=Config._ENDPOINT_DB_CHECK,headers=headers,json=body)
+
+        if response.status_code == 200:
             response = response.json()
-            print('question_id',response['question_id'])
-            self.question_id=response['question_id']
-            if response["Output type"] == "TABLE":
-                response["Output"] = pd.DataFrame(response["Output"]["data"],columns=response["Output"]["columns"])
-                if 'index' in response["Output"].columns:
-                    response["Output"].drop(["index"],axis=1,inplace=True)
-            return Answer(response['question_id'],response["Status"],response["SQL"],response["Output"],response['Output chart'])
+            if response["Status"] == "Success":
+              return response["all_tables"]
+            else:
+              print(f'Status: {response["Status"]}, Error message: {response["Output"]}')
         else:
-            response
-    
-    def flag_question(self,flag: str,question_id=None):
-        """Flag the question if the answer was not correct.
+            raise Exception(f'Error creating project: {response.text}')
 
-        Args:
-            flag (str): Text that describes the situation, the correct SQL is possible.
-            question_id (str, optional): The id of the question which you want to flag. If not provided the last question is flagged. Defaults to None.
 
-        Returns:
-            response: the response of the request
-        """
-        headers = {"api_key":self.API_KEY}
-        if question_id:
-            body = {"question_id":question_id,'flag':flag}
+    def create_project(project_name, input_type=Config.TYPE_CHAT, **kwargs):
+
+        headers = {"api_key": Config.API_KEY} 
+        body = {"input_type": input_type, "project_name": project_name}
+
+        if input_type == Config.TYPE_URL:
+          body.update({"urls":kwargs.get("urls"), "crawl":kwargs.get("crawl")})
+        elif input_type in [Config.TYPE_PDF,Config.TYPE_CSV]:
+          body.update({"data":kwargs.get("data")})
+        elif input_type == Config.TYPE_DB:
+          body.update({"connection_string":kwargs.get("connection_string"), "table_names":kwargs.get("table_names")})
+        
+        response = requests.post(url=Config._ENDPOINT_TRAINING,headers=headers,json=body)
+        
+        if response.status_code == 200:
+            response = response.json()
+            if response["Status"] == "Success":
+              return response["Output"]
+            else:
+              print(f'Status: {response["Status"]}, Error message: {response["Output"]}')
         else:
-            body = {"question_id":self.question_id,'flag':flag}
-        response = requests.post(url=self._ENDPOINT_QUESTION_FLAG,headers=headers,json=body)
+            raise Exception(f'Error creating project: {response.text}')
+
+
+class Ask(Config):
+
+    def ask_question_base(question, project_key, endpoint, **kwargs):
+        headers = {'api_key': Config.API_KEY}
+        body = {"project_key":project_key,"question":question}
+
+        response = requests.post(endpoint, headers=headers, json=body)
+        if response.status_code == 200:
+            response = response.json()
+            if response["Status"] == "Success":
+              return response
+            else:
+              print(f'Status: {response["Status"]}, Error message: {response["Error_message"]}')
+        else:
+            raise Exception(f'Error asking question: {response.text}')
+
+    def ask_chat(question, project_key, endpoint=Config._ENDPOINT_QUESTION):
+        response = Ask.ask_question_base(question, project_key, endpoint)
+        return response["Output"]
+
+    def ask_document(question, project_key, endpoint=Config._ENDPOINT_QUESTION_DOC):
+        response = Ask.ask_question_base(question, project_key, endpoint)
+        return response["Output"]
+
+    def ask_tabular(question, project_key, endpoint=Config._ENDPOINT_QUESTION):
+        response = Ask.ask_question_base(question, project_key, endpoint)
+        
+        if not response["Output"]:
+            required_keys = ["SQL"]
+        else:
+            required_keys = ["Output","Output type", "SQL"]
+            if response["Chart exist"]:
+                required_keys.append("Output Chart")
+        response = {i:response[i] for i in required_keys}
         return response
-            
-            
-    # metadata is prohect level now
-    def send_metadata(self,metadata):
-        
-        """
-        Sends metadata and project key to API for training.
-        
-        Args:
-            metadata (dict): Key value pairs for acronyms or other information matching.
-        
-        Returns:
-            Status (str): Status to make sure metadata was added successfully.    
-        """
-        headers = {"api_key":self.API_KEY}
-        body = {"project_key":self.PROJECT_KEY,"metadata":str(metadata)}
-        response = requests.post(url=Anania._ENDPOINT_METADATA,headers=headers,json=body)
-        
-        return response.json()             
-        
-    def delete_project(self,project_key=None):
-        """Delete an existing project.
-        Args:
-            project_key (str, optional): The key of the project to be deleted.If not provided the current project is deleted. Defaults to None.
 
-        Returns:
-            response: the response of the request
-        """
-        headers = {"api_key":self.API_KEY}
-        if project_key:
-            body = {"project_key":project_key}
+    def ask(question, project_type, project_key, **kwargs):
+        if project_type in [Config.TYPE_CSV,Config.TYPE_DB]:
+            Ask.ask_tabular(question, project_key)
+        elif project_type in [Config.TYPE_PDF, Config.TYPE_URL]:
+            Ask.ask_document(question, project_key)
+        elif project_type in [Config.TYPE_CHAT]:
+            Ask.ask_chat(question, project_key)
         else:
-            body = {"project_key":self.project_key}
-        response = requests.delete(url=self._ENDPOINT_DELETE_PROJECT,headers=headers,json=body)
-        return response
-        
-        
-    def get_all_tables_db(self,connection_string):
-        """Connects to tb and return the list of all tables available in the db
-
-        Args:
-            connection_string (str): the db connection string
-
-        Returns:
-            response: the response of the request
-            all_tables (list,None) : the list of all tables in the db, None if the connection was not succesfull
-        """
-        headers = {"api_key":self.API_KEY}
-        body = {"connection_string":connection_string}
-        response = requests.post(url=Anania._ENDPOINT_DB_CHECK,headers=headers,json=body)
-        if response.status_code==200:
-            response=response.json()
-            all_tables = response['all_tables']
-        else:
-            all_tables=None
-        return response,all_tables
-        
-    def update_api_key(self):
-        """Updates the user api key.
-
-        Returns:
-            response: response of the request
-        """
-        headers = {"api_key":self.API_KEY}
-        response = requests.post(url=Anania._ENDPOINT_UPDATE_API_KEY,headers=headers)
-        if response.status_code ==200:
-            if response.json()['Status']=='Success':
-                self.API_KEY = response.json()['new_api_key']
-        return response
+            raise Exception(f"Unsupported project type: {project_type}")
