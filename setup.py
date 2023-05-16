c84f7c8ab15ceb7e60dd36dbafce5d90c84cda19
Metric <48058391+Metricam@users.noreply.github.com>
2023-05-16 16:06:26 +0400

    Remove find_packages() from setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="Anania",
     version="0.1",
-    packages=find_packages(),
     description="Python client for interacting with Anania API.",
     author="Team Anania",
     author_email="hrant@anania.ai",
