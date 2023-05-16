0524164e54530ca652eb8dec2ee71f2cf1134f6c
Metric <48058391+Metricam@users.noreply.github.com>
2023-05-16 10:53:52 +0400

    Updated demo using Anania class instead of Create and Ask

--- demo.py
+++ demo.py
@@ -1,10 +1,10 @@
-from anania import Config, Create, Ask
+from anania import Config, Anania
 
 Config.API_KEY = "YOUR API KEY"
 
 ## Using existing project
-Ask.ask_document(question = "YOUR QUESTION", project_key = "YOUR PROJECT KEY")
+Anania.ask_document(question = "YOUR QUESTION", project_key = "YOUR PROJECT KEY")
 
 ## Creating a new project and using it
-project_key_url = Create.create_project_url(project_name = "YOUR PROJECT NAME", urls = ["https://metric.am/"], crawl = False)
-Ask.ask_document("YOUR QUESTION", project_key = project_key_url)
+project_key_url = Anania.create_project_url(project_name = "YOUR PROJECT NAME", urls = ["https://metric.am/"], crawl = False)
+Anania.ask_document("YOUR QUESTION", project_key = project_key_url)
